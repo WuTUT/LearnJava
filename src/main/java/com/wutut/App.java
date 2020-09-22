3dcff49c063fda4885d5bf12c3570f308674efa0
WuTUT <762641171@qq.com>
2020-09-22 15:17:32 +0800

    learn maven

--- src/main/java/com/wutut/App.java
+++ src/main/java/com/wutut/App.java
@@ -1,5 +1,11 @@
 package com.wutut;
 
+// import net.sourceforge.pinyin4j.PinyinHelper;
+// import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+// import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+// import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+// import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 /**
  * Hello world!
  */
@@ -9,9 +15,21 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // String s = "你好啊， 我是谁";
+        // HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        // format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        // format.setVCharType(HanyuPinyinVCharType.WITH_U_UNICODE);
+        // try {
+        // String ret = PinyinHelper.toHanYuPinyinString(s, format, " ", true);
+        // System.out.println(ret);
+        // } catch (BadHanyuPinyinOutputFormatCombination e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
     }
 }
